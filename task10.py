1a37d35bc498d9895dd7c9ceef6802b40cf43f6d
Лаврова Елизавета Александровна <e.lavrova2@main.nsu.ru>
2024-10-14 14:08:26 +0700

    10 не сделала

--- task10.py
+++ task10.py
@@ -4,4 +4,38 @@
 # Если таких чисел не было, вывести "нет".
 
 
+# когда_нибудь
+'''
+def check_prostoe(x):
+    q = x // 2
+    d = [1, x]
+    for i in range(2, q + 1):
+        if x % i == 0:
+            d.append(i)
+    if len(d) == 2:
+        return x
 
+
+n = int(input())
+minn = int(input())
+maxx = minn
+a = 0
+while check_prostoe(minn) is None and a < n:
+    minn = int(input())
+    a += 1
+for _ in range(n - a):
+    j = check_prostoe(int(input()))
+    if j is None:
+        continue
+    else:
+        if j <= minn:
+            minn = j
+        elif j >= maxx:
+            maxx = j
+
+
+print(f'Наибольшее: {maxx}, наименьшее: {minn}')
+'''
+
+# нужно сделать проверку на отсутствие подходящих чисел,
+# а то оно вообще не реагирует на составные
